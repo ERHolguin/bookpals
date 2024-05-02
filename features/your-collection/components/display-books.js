7f11b2fa5fb892155db6a3bbcae00b6e57ba4834
ERHolguin <rrodshedale@gmail.com>
2024-05-02 17:57:19 -0400

    Create Array for User's Collection

---
+++ features/your-collection/components/display-books.js
@@ -0,0 +1,7 @@
+/*const personalCollection = document.getElementById("myBookShelf")*/
+
+const personalCollection = [
+    "Grammatica de Uso Del espanol: teoria y pratica A1-B2 (Spanish edition",
+    "Pour Lire avec Plaisir"
+];
+
