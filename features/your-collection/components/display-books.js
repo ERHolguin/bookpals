356cea9d9532214c551b9909f8bb6b1fb21699ba
ERHolguin <rrodshedale@gmail.com>
2024-05-03 11:20:48 -0400

    Add Sample Book to userBookshelf

--- features/your-collection/components/display-books.js
+++ features/your-collection/components/display-books.js
@@ -1,11 +1,24 @@
-/*const personalBookshelf = document.getElementById("myBookShelf")*/
+/*const userBookshelf = document.getElementById("myBookShelf")*/
 
-const personalBookshelf = [
+const userBookshelf = [
     "Grammatica de Uso Del espanol: teoria y pratica A1-B2 (Spanish edition",
     "Pour Lire avec Plaisir"
 ];
 
-function Book(author, title) {
+function Book(title, author) {
     this.author = author;
     this.title = title;
+    this.addBookDetails = function() {
+        return this.title + ": " + this.author;
+    };
+
 };
+
+function addBookToUserBookshelf() {
+    const book = new Book("Pride & Prejudice", "Jane Austin");
+    const bookDetails = book.addBookDetails();
+    return userBookshelf.push(bookDetails);
+};
+
+addBookToUserBookshelf();
+console.log(userBookshelf);
