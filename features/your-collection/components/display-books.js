fbe0bb3a008bbcbc22ec986d14300fe8f4d1fe8d
ERHolguin <rrodshedale@gmail.com>
2024-05-03 10:33:03 -0400

    Create Book Constructor

--- features/your-collection/components/display-books.js
+++ features/your-collection/components/display-books.js
@@ -1,7 +1,11 @@
-/*const personalCollection = document.getElementById("myBookShelf")*/
+/*const personalBookshelf = document.getElementById("myBookShelf")*/
 
-const personalCollection = [
+const personalBookshelf = [
     "Grammatica de Uso Del espanol: teoria y pratica A1-B2 (Spanish edition",
     "Pour Lire avec Plaisir"
 ];
 
+function Book(author, title) {
+    this.author = author;
+    this.title = title;
+};
