30a2025bcfd3ecfecfe45aa777fee88437ee46ea
ERHolguin <rrodshedale@gmail.com>
2024-05-03 15:10:51 -0400

    Add Book Card Elements

--- features/your-collection/components/display-books.js
+++ features/your-collection/components/display-books.js
@@ -1,26 +1,46 @@
+let card;
 let userBookShelf = document.getElementById("userBookShelf");
 
 /*userBookshelf = [];*/
 
-function Book(title, author) {
-    this.author = author;
-    this.title = title;
-    this.addBookDetails = function() {
-        return this.title + ": " + this.author;
-    };
-
-};
-
 function createBookCard(card) {
     for(let i = 0; i < card; i++) {
         let card = document.createElement("div");
-        card.className = "book-card";
-        card.textContent = "";
+            card.className = "book-card";
+            card.textContent = "";
+        
+        let bookCover = document.createElement("div");
+            bookCover.className = "book-cover";
+
+        let bookDetails = document.createElement("div");
+            bookDetails.className = "book-details";
+            let bookTitle = document.createElement("title");
+                bookTitle.className = "book-title"
+                bookTitle.textContent = "";
+            let bookAuthor = document.createElement("title");
+                bookAuthor.className = "book-author"
+                bookAuthor.textContent = "";
+        
+        card.appendChild(bookCover);
+        card.appendChild(bookDetails);
+        bookDetails.appendChild(bookTitle);
+        bookDetails.appendChild(bookAuthor);
         userBookShelf.appendChild(card);
     };
 };
 
-createBookCard(1);
+createBookCard(4);
+
+function Book(title, author) {
+    this.author = author;
+    this.title = title;
+    /*this.addBookDetails = function() {
+        return this.title + ": " + this.author;
+    };*/
+
+};
+
+
 
 /*
 function addBookToUserBookshelf() {
